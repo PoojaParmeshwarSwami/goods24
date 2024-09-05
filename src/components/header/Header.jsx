6f5953d439b48dca357c619c8ab4b5d4758ffadc
Pooja Parmeshwar Swami <poojaswami8316@gmail.com>
2024-09-05 14:18:00 +0530

    searchbar working with image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,39 @@
 import { LiaShoppingCartSolid } from "react-icons/lia";
 import { CiSettings } from "react-icons/ci";
 import { FaRegUser } from "react-icons/fa";
-
 import { CgProfile } from "react-icons/cg";
 import { TbLogout2 } from "react-icons/tb";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "/src/assets/logo.png";
 import "/src/components/header/header.css";
 import axios from "axios";
 
+
 const HeaderComponent = () => {
-  const [openModal, setOpenModal] = useState(false);
   const [searchBox, setSearchBox] = useState(false);
   const [inputval, setInputVal] = useState("");
- const[data,setData]= useState("");
+  const [data, setData] = useState([]);
+  const[openModal,setOpenModal]= useState(false)
+  
   const navigate = useNavigate();
+
   const handleLogout = () => {
     localStorage.removeItem("info");
     navigate("/login");
   };
 
-
   const fetchdata = async (value) => {
-
     try {
-        const finalData = {
-            search:value,
-          };
-        
+      if (value.trim() === "") {
+        setData([]); // Clear results if input is empty
+        setSearchBox(false); // Hide search results if input is empty
+        return;
+      }
+
+      const finalData = {
+        search: value,
+      };
 
       const response = await axios.get(
         "https://goods24api.yelmarpariwar.com/website/product/search/18.4509994/73.827328",
@@ -43,13 +48,16 @@ const HeaderComponent = () => {
       if (response.data.status) {
         setData(response.data.data);
         console.log("Search data", response.data.data);
+        setSearchBox(true); // Show search results if data is fetched
       }
     } catch (error) {
       console.log("search error", error);
+      setSearchBox(false); // Hide search results in case of error
     }
   };
 
-  const handleChange = (value) => {
+  const handleChange = (e) => {
+    const value = e.target.value;
     setInputVal(value);
     fetchdata(value);
   };
@@ -58,18 +66,16 @@ const HeaderComponent = () => {
     <>
       <div className="headerDiv">
         <Link to="/">
-          {
-            <img
-              src={logo}
-              alt=""
-              style={{
-                width: "220px",
-                mixBlendMode: "darken",
-                height: "60px",
-                marginTop: "5px",
-              }}
-            />
-          }
+          <img
+            src={logo}
+            alt=""
+            style={{
+              width: "220px",
+              mixBlendMode: "darken",
+              height: "60px",
+              marginTop: "5px",
+            }}
+          />
         </Link>
 
         <input
@@ -77,26 +83,37 @@ const HeaderComponent = () => {
           type="text"
           value={inputval}
           placeholder="Search"
-          onClick={() => {
-            setSearchBox(!searchBox);
-          }}
-          onChange={(e) => {
-            handleChange(e.target.value);
-          }}
+          onChange={handleChange}
         />
-        {searchBox && <div className="searchBoxDiv"> {
-            data.map
-            }</div>}
+        
+        {searchBox && (
+          <div className="search-results">
+            {data.length > 0 ? (
+              <>
+                {data.map((item) => (
+                  <div key={item.id}>
+                    <Link to={`/product/${item.id}`} className="search-result-item">
+                  <div>
+                    <img src={item.product_image} alt="" style={{height:'30px',width:'30px',borderRadius:'50px'}} />
+                    {item.product_name}  {item.type}
+                    </div>
+                    </Link>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <p>No results found</p>
+            )}
+          </div>
+        )}
 
         <button className="search-button">Search</button>
 
         <Link onClick={() => setOpenModal(!openModal)}>
-          {
-            <CgProfile
-              className="header-icons"
-              style={{ marginLeft: "95px", height: "30px", marginTop: "20px" }}
-            />
-          }
+          <CgProfile
+            className="header-icons"
+            style={{ marginLeft: "95px", height: "30px", marginTop: "20px" }}
+          />
         </Link>
         {openModal && (
           <div className="logOutmodal">
@@ -122,4 +139,5 @@ const HeaderComponent = () => {
     </>
   );
 };
+
 export default HeaderComponent;
